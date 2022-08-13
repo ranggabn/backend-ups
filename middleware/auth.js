9a8e16d245b576a0ebbaa5085947df729c336578
Rangga Baghas <66686493+ranggabn@users.noreply.github.com>
2022-08-13 17:33:21 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 var connection = require("../koneksi");
 var mysql = require("mysql");
-var md5 = require("MD5");
+var md5 = require("md5");
 var response = require("../res");
 var jwt = require("jsonwebtoken");
 var config = require("../config/secret");
